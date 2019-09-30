49e50f28b009bd713fb6755d81ed971f3b63cc8b
root <chinenyediei@gmail.com>
2019-09-30 14:50:16 -0500

    final draft

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# friend_finder
+# Friend Finder
+
+#
+##About the App
+
+The app starts by asking the user to enter their name and enter a url link to a photo of them. This information will be stored in a link on the website "API Friends List". This will allow other users to match with users who submitted information before them. The program does this by asking the user to answer 10 questions about their interests and lifestlyes. The answers for the questions are ranked from 1 to 5, 1 being that the question is least like them or 5 if they strongly agree and feel that this question is much like them. Once they have filled out the survey they are matched with the person who has the most in common with them. 
+
+##Starting the app
+To start the app you will need to install a few packages, run the following commands to get the app started:
+
+* npm init -y
+* npm install express
+* npm install 
+
+##Languages Used
+* JavaScript
+* JQuery
+* node.js
+* Express
+* HTML
+* Bootstrap
+
+
+[Click here to see the app!](https://wctcprog98.github.io/friend_finder/)
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,108 +1,102 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
-var tableArray = [
+var friends = [
   {
-    "name":"Ahmed",
+    "name":"Homer Simpson",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
         1,
-        4,
-        4,
-        5,
+        3,
         1,
         2,
         5,
+        2,
+        1,
         4,
         1
       ]
   },
   {
-    "name":"Ahmed",
+    "name":"Marge Simpson",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
+        2,
+        4,
+        3,
+        4,
+        5,
+        3,
+        2,
+        4,
+        5
+      ]
+  },
+  {
+    "name":"Bart Simpson",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
         1,
         4,
         4,
-        5,
+        4,
         1,
         2,
-        5,
+        3,
         4,
         1
       ]
   },
   {
-    "name":"Ahmed",
+    "name":"Lisa Simpson",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
+        3,
         1,
+        3,
         4,
-        4,
-        5,
         1,
         2,
-        5,
+        2,
         4,
-        1
+        5,
+        3
       ]
   },
   {
-    "name":"Ahmed",
+    "name":"MaggieSimpson ",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
         5,
-        1,
+        7,
         4,
         4,
-        5,
-        1,
+        3,
+        2,
         2,
         5,
         4,
-        1
+        2
       ]
   },
   {
-    "name":"Ahmed",
+    "name":"Principal Skinner",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
         2,
-        5,
-        4,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
+        1,
+        3,
         5,
         1,
-        4,
-        4,
         5,
-        1,
         2,
-        5,
         4,
-        1
+        4,
+        3
       ]
   }
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = tableArray;
+module.exports = friends;
+
